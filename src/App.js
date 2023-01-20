4882fca7fe86ccc8163d55499e0da2edf1128037
Frazier Odhiambo <ofrazy@gmail.com>
2023-01-21 02:00:03 +0300

    initial commit to navbar

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import { Box } from "@mui/material"
+
+import { Navbar, Feed, VideoDetail, ChannelDetail, SearchFeed } from './components' //import components from folder in a consice and clear manner
+
+
+const App = () => (
+    <BrowserRouter>
+      <Box sx={{ backgroundColor: '#000' }}>
+        <Navbar />
+        <Routes>
+          <Route path="/" exact element={<Feed />} />
+          <Route path="/video/:id" element={<VideoDetail />} />
+          <Route path="/channel/:id" element={<ChannelDetail />} />
+          <Route path="/search/:searchTerm" element={<SearchFeed />} />
+        </Routes>
+      </Box>
+
+    </BrowserRouter>
+  )
+
+
+export default App
---
+++ src/components/index.js
@@ -0,0 +1,8 @@
+export {default as Navbar} from './Navbar'
+export {default as Feed} from './Feed'
+export {default as ChannelDetail} from './ChannelDetail'
+export {default as SearchFeed} from './SearchFeed'
+export {default as VideoDetail} from './VideoDetail'
+
+
+//these components will be imported to the App.js file which reduces the clutter from the App.js folder making it easier to understand and edit
