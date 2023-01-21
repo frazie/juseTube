c4cea2e94614e8e6291ed00b5583bc0fdb268e7a
Frazier Odhiambo <ofrazy@gmail.com>
2023-01-21 23:38:58 +0300

    react-branch initiated

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,7 +8,7 @@ const Navbar = () => (
         direction='row' 
         alignItems='center' 
         p={2} 
-        sx={{position: 'sticky', background:'#000', top:'0', justifyContent:'space-between'}}
+        sx={{position: 'sticky', background: '#000', top: '0', justifyContent: 'space-between'}}
     >
         <Link to='/' style={{display: 'flex', alignItems: 'center' }}>
         
