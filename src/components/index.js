2c43947af110bc1b2c9426d854da7c03222ad474
Frazier Odhiambo <ofrazy@gmail.com>
2023-01-23 01:01:31 +0300

    nav bar added

--- src/components/index.js
+++ src/components/index.js
@@ -3,6 +3,7 @@ export {default as Feed} from './Feed'
 export {default as ChannelDetail} from './ChannelDetail'
 export {default as SearchFeed} from './SearchFeed'
 export {default as VideoDetail} from './VideoDetail'
+export {default as SearchBar} from './SearchBar'
 
 
 //these components will be imported to the App.js file which reduces the clutter from the App.js folder making it easier to understand and edit
