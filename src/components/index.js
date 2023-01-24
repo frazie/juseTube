ac01733678442d7a37b69bb7e49a547f9fce9c78
Frazier Odhiambo <ofrazy@gmail.com>
2023-01-24 18:12:57 +0300

    issue-4--add-page--feed

--- src/components/index.js
+++ src/components/index.js
@@ -5,6 +5,8 @@ export {default as SearchFeed} from './SearchFeed'
 export {default as VideoDetail} from './VideoDetail'
 export {default as SearchBar} from './SearchBar'
 export {default as Sidebar} from './Sidebar'
+export {default as Videos} from './Videos'
+
 
 
 
