ca790bcd7cd822d010f1f59fb0c5492d752c7e0e
Frazier Odhiambo <ofrazy@gmail.com>
2025-04-21 20:05:46 +0300

    Update VideoCard.jsx

--- src/components/VideoCard.jsx
+++ src/components/VideoCard.jsx
@@ -2,7 +2,7 @@ import { Link } from 'react-router-dom'
 import { Typography, Card, CardContent, CardMedia } from '@mui/material'
 import { CheckCircle } from '@mui/icons-material'
 
-import { demoThumbnailUrl, demoVideoUrl, demoVideoTitle, demoChannelUrl, demoChannelTitle } from '../utils/constants'
+import { demoVideoUrl, demoVideoTitle, demoChannelUrl, demoChannelTitle } from '../utils/constants'
 
 const VideoCard = ({ video: { id : { videoId }, snippet } }) => {
     
